4f9d8bb1dd0762179010740a699e7a16e6949b86
Brian Perry <brian.perry@pantheon.io>
2023-05-01 10:43:59 -0500

    Hardcode initial locale to resolve routing issues during build

--- lib/get-global-elements.ts
+++ lib/get-global-elements.ts
@@ -59,7 +59,7 @@ export async function getGlobalElements(
   })
 
   return {
-    ...(await serverSideTranslations(context.locale, ["common"])),
+    ...(await serverSideTranslations('en', ["common"])),
     menus: {
       main: mainMenu.items,
       footer: footerMenu.items,
